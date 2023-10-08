dfedeac6ccf3c037d08ee329a7911221a1077394
Cristian Braga <cobraga@aligera.com.br>
2023-10-08 15:23:27 -0300

    inicio

---
+++ main.js
@@ -0,0 +1,50 @@
+let listaDeItens = []
+
+const form = document.getElementById("form-itens")
+const itensInput = document.getElementById("receber-item")
+const ulItens = document.getElementById("lista-de-itens")
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    salvarItem()
+    mostrarItens()
+})
+
+function salvarItem() {
+    const comprasItem = itensInput.value;
+    const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase())
+
+    if (checarDuplicado) {
+        alert("Item já existe.")
+    } else {
+         listaDeItens.push({
+            valor: comprasItem,
+        })
+    }
+
+    console.log(listaDeItens)
+}
+
+function mostrarItens() {
+    ulItens.innerHTML = ""
+  listaDeItens.forEach((elemento, index) => {
+    ulItens.innerHTML +=
+    `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+    <div>
+        <input type="checkbox" class="is-clickable" />
+        <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+    </div>
+    <div>
+        <i class="fa-solid fa-trash is-clickable deletar"></i>
+    </div>
+    </li>`})
+
+    const inputsCheck = documents.querySelectorAll('input[type-checkbox]')
+
+    inputsCheck.forEach(i => {
+      i.addEventListener('click', (evento) => {
+        console.log("Fui clicado")
+      })
+    })
+
+}
